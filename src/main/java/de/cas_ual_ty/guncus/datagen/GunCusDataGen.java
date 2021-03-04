3380142b20bd4567a1c91059d24382475c9ffd2c
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2021-03-04 17:59:12 +0100

    Deleted unneeded block states and item models

--- src/main/java/de/cas_ual_ty/guncus/datagen/GunCusDataGen.java
+++ src/main/java/de/cas_ual_ty/guncus/datagen/GunCusDataGen.java
@@ -13,9 +13,9 @@ public class GunCusDataGen
     public static void gatherData(GatherDataEvent event)
     {
         DataGenerator generator = event.getGenerator();
-        generator.addProvider(new GunCusGunModels(generator, GunCus.MOD_ID, event.getExistingFileHelper()));
-        generator.addProvider(new GunCusItemModels(generator, GunCus.MOD_ID, event.getExistingFileHelper()));
         generator.addProvider(new GunCusBlockModels(generator, GunCus.MOD_ID, event.getExistingFileHelper()));
         generator.addProvider(new GunCusBlockStates(generator, GunCus.MOD_ID, event.getExistingFileHelper()));
+        generator.addProvider(new GunCusGunModels(generator, GunCus.MOD_ID, event.getExistingFileHelper()));
+        generator.addProvider(new GunCusItemModels(generator, GunCus.MOD_ID, event.getExistingFileHelper()));
     }
 }
