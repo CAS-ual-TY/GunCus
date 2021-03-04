afee06453e20fc5b9fcf4480744f0eb4017f5ab4
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2021-03-04 15:37:50 +0100

    Fixed gun models datagen

--- src/main/java/de/cas_ual_ty/guncus/datagen/GunCusGunModels.java
+++ src/main/java/de/cas_ual_ty/guncus/datagen/GunCusGunModels.java
@@ -1,23 +1,49 @@
 package de.cas_ual_ty.guncus.datagen;
 
+import javax.annotation.Nonnull;
+
+import com.google.common.base.Preconditions;
+
 import de.cas_ual_ty.guncus.item.AttachmentItem;
 import de.cas_ual_ty.guncus.item.GunItem;
 import net.minecraft.data.DataGenerator;
-import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ModelFile.UncheckedModelFile;
+import net.minecraftforge.client.model.generators.ModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
-public class GunCusGunModels extends ItemModelProvider
+public class GunCusGunModels extends ModelProvider<ItemModelBuilder>
 {
     public GunCusGunModels(DataGenerator generator, String modid, ExistingFileHelper existingFileHelper)
     {
-        super(generator, modid, existingFileHelper);
+        super(generator, modid, ITEM_FOLDER, GunCusItemModelBuilder::new, existingFileHelper);
     }
     
+    @Nonnull
     @Override
     public String getName()
     {
-        return "GunCus Gun Item Models";
+        return "Item Models: " + this.modid;
+    }
+    
+    public static class GunCusItemModelBuilder extends ItemModelBuilder
+    {
+        public GunCusItemModelBuilder(ResourceLocation outputLocation, ExistingFileHelper existingFileHelper)
+        {
+            super(outputLocation, existingFileHelper);
+        }
+        
+        @Override
+        public GunCusItemModelBuilder texture(String key, ResourceLocation texture)
+        {
+            // Overriding this because apparently gun attachment textures cant be found
+            // so this method would throw an Exception if not overridden
+            Preconditions.checkNotNull(key, "Key must not be null");
+            Preconditions.checkNotNull(texture, "Texture must not be null");
+            this.textures.put(key, texture.toString());
+            return this;
+        }
     }
     
     @Override
@@ -27,13 +53,17 @@ public class GunCusGunModels extends ItemModelProvider
         
         for(GunItem gun : GunItem.ALL_GUNS_LIST)
         {
+            this.getBuilder(gun.getRegistryName().toString())
+                .parent(new UncheckedModelFile("item/generated"))
+                .texture("layer0", this.modLoc("item/dummy"));
+            
             for(AttachmentItem[] attachments : gun.getAttachments())
             {
                 for(AttachmentItem attachment : attachments)
                 {
                     path = gun.getRegistryName().getPath() + "/" + attachment.getRegistryName().getPath();
                     
-                    this.getBuilder(this.modid + ":" + path)
+                    this.getBuilder(this.modid + ":item/" + path)
                         .parent(new UncheckedModelFile("item/generated"))
                         .texture("layer0", this.modLoc("item/" + path));
                 }
